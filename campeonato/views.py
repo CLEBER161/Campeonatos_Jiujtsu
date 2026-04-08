c2fe657a1f73fa51cc052a6d3e39ce29a00c238e
cleber <cleber.v499@gmail.com.br>
2026-04-07 21:17:43 -0300

    fix: simplificar tela de estrutura e corrigir import User em views

--- campeonato/views.py
+++ campeonato/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.db import transaction
 from django.db.models import Q, Count
 from django.http import JsonResponse
